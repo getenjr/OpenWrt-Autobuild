eaa201fe92492dbb98c6a6ae8dfd985a77a452e9
getenjr <32555548+getenjr@users.noreply.github.com>
2024-07-27 07:48:54 +0700

    Update 06_cleaning.sh

--- SCRIPTS/06_cleaning.sh
+++ SCRIPTS/06_cleaning.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 rm -rf `ls | grep -v "rootfs"`
+ls *rootfs.img.gz 1> /dev/null 2>&1
 ls *rootfs.img.gz | xargs rm -fr
+ls *.gz 1> /dev/null 2>&1
 gzip -d *.gz
+ls *.img 1> /dev/null 2>&1
 gzip *.img
+ls *.gz 1> /dev/null 2>&1
 for gzfirm in *.gz; do
   sha256sum "$gzfirm" >> sha256sum.txt
 done
+[ -f ../../../../.config ]
 mv ../../../../.config config-full
 
 exit 0
